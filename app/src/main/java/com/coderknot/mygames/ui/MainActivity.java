c0945d5949569d85bc6961dfcf6480e523da4c30
Chris Carr <coder.knot@gmail.com>
2017-06-20 15:36:14 -0700

    update to use pager for tabs

--- app/src/main/java/com/coderknot/mygames/ui/MainActivity.java
+++ app/src/main/java/com/coderknot/mygames/ui/MainActivity.java
@@ -1,15 +1,45 @@
 package com.coderknot.mygames.ui;
 
 import android.os.Bundle;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 
 import com.coderknot.mygames.R;
+import com.coderknot.mygames.adapters.SectionsPageAdapter;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = MainActivity.class.getSimpleName();
+
+    private SectionsPageAdapter mSectionsPageAdapter;
+    private ViewPager mViewPager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Log.d(TAG, "onCreate: Starting...");
+
+        mSectionsPageAdapter = new SectionsPageAdapter(getSupportFragmentManager());
+
+        mViewPager = (ViewPager) findViewById(R.id.container);
+        setupViewPager(mViewPager);
+
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.tabs);
+        tabLayout.setupWithViewPager(mViewPager);
+
     }
+
+    private void setupViewPager(ViewPager viewPager) {
+        SectionsPageAdapter adapter = new SectionsPageAdapter(getSupportFragmentManager());
+        adapter.addFragment(new HomeFragment(), "Home");
+        adapter.addFragment(new OwnedFragment(), "Owned List");
+        adapter.addFragment(new WishlistFragment(), "Wish List");
+
+        viewPager.setAdapter(adapter);
+    }
+
 }
+
